61bf156e8015e3b83d4a3d4b3117ce5ceac16baa
2022wa86490-eng <2022wa86490@wilp.bits-pilani.ac.in>
2026-09-22 17:21:04 +0530

    App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,7 +2,7 @@ public class App {
 
     public static void main(String[] args) {
 
-        System.out.println("2022wa86490");
+        System.out.println("continuos integration succesfull");
 
     }
 
